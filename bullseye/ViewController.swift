227e83264dedf77da2a62c6d23a8b7ca90a22663
meljohndelbarrio <meljdelbarrio@yahoo.com>
2017-05-15 15:36:33 +1200

    Connected the button to the ViewController and a created a showAlert() that prints "Hello iOS" to the console

--- bullseye/ViewController.swift
+++ bullseye/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func showAlert(_ sender: Any) {
+        print("Hello iOS")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
