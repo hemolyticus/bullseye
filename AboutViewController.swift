5787a4a4e838da3192c2450de8c047db4024efe6
meljohndelbarrio <meljdelbarrio@yahoo.com>
2017-05-16 15:30:50 +1200

    Added an IBAction called  close that dismisses the About screen when the close button is clicked

--- AboutViewController.swift
+++ AboutViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class AboutViewController: UIViewController {
 
+    
+    @IBAction func close()
+    {
+        dismiss(animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
