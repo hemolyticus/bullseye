dc90a91c762e7d64c9779798232b025a062a13d5
meljohndelbarrio <meljdelbarrio@yahoo.com>
2017-05-17 15:47:56 +1200

    Added an html file that will contain the game instructions. Added a web view  into the 2nd screen and added the contents of the html file into at ViewDidLoad()

--- AboutViewController.swift
+++ AboutViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class AboutViewController: UIViewController {
 
-    
+    @IBOutlet weak var webView: UIWebView!
+        
     @IBAction func close()
     {
         dismiss(animated: true, completion: nil)
@@ -18,7 +19,13 @@ class AboutViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    if let url = Bundle.main.url(forResource: "BullsEye", withExtension: "html")
+    {
+        if let htmlData = try? Data(contentsOf: url){
+            let baseUrl = URL(fileURLWithPath: Bundle.main.bundlePath)
+            webView.load(htmlData, mimeType: "text/html", textEncodingName: "UTF-8", baseURL: baseUrl)
+        }
+        }
     }
 
     override func didReceiveMemoryWarning() {
